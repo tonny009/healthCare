b4b39806665953b8a57c438fb9213916b7cf43f1
tabassum009 <tabassum.islam009@gmail.com>
2026-07-25 16:42:15 +0200

    Prisma install and project  basic setup done

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express, { Application, Request, Response } from "express";
+//import { prisma } from "./app/lib/prisma";
+//import { IndexRoutes } from "./app/routes";
+
+const app: Application = express();
+
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+//app.use("/api/v1", IndexRoutes);
+
+// Basic route
+app.get('/', async (req: Request, res: Response) => {
+
+    res.send('API is working');
+
+    /*const specialty = await prisma.specialty.create({
+        data: {
+            title: 'Cardiology'
+        }
+    })
+    res.status(201).json({
+        success: true,
+        message: 'API is working',
+        data: specialty
+    })*/
+});
+
+export default app;
