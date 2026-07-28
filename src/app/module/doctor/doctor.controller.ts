9617be329341a245d4fae018e2a4ca45c984a959
tabassum009 <tabassum.islam009@gmail.com>
2026-07-28 12:39:44 +0200

    doctor crud

--- src/app/module/doctor/doctor.controller.ts
+++ src/app/module/doctor/doctor.controller.ts
@@ -18,13 +18,55 @@ const getAllDoctors = catchAsync(
     }
 )
 
-// const getDoctorById = catchAsync(
-//const updateDoctor = catchAsync(
-//const deleteDoctor = catchAsync(
+const getDoctorById = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const doctor = await DoctorService.getDoctorById(id as string);
+
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "Doctor fetched successfully",
+            data: doctor,
+        })
+    }
+)
+
+const updateDoctor = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const payload = req.body;
+
+        const updatedDoctor = await DoctorService.updateDoctor(id as string, payload);
+
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "Doctor updated successfully",
+            data: updatedDoctor,
+        })
+    }
+)
+
+const deleteDoctor = catchAsync(
+    async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const result = await DoctorService.deleteDoctor(id as string);
+
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "Doctor deleted successfully",
+            data: result,
+        })
+    }
+)
 
 export const DoctorController = {
     getAllDoctors,
-    // getDoctorById,
-    // updateDoctor,
-    // deleteDoctor,
+    getDoctorById,
+    updateDoctor,
+    deleteDoctor,
 };
