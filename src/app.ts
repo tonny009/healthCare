f8c917f9162a03a15277723b0a94fbb5313c98a7
tabassum009 <tabassum.islam009@gmail.com>
2026-09-05 22:47:48 +0200

    cloudinary image uploading done

--- src/app.ts
+++ src/app.ts
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", IndexRoutes);
 
--- src/app/module/specialty/specialty.route.ts
+++ src/app/module/specialty/specialty.route.ts
@@ -2,12 +2,17 @@ import { Router } from "express";
 import { SpecialtyController } from "./specialty.controller";
 import { checkAuth } from "../../middleware/checkAuth";
 import { Role } from "../../../generated/prisma/browser";
-//import { SpecialtyController } from "./specialty.controller";
+import { multerUpload } from "../../config/multer.config";
+import { validateRequest } from "../../middleware/validateRequest";
+import { SpecialtyValidation } from "./specialty.validation";
 
 const router = Router();
 
-router.post('/', checkAuth(Role.PATIENT), SpecialtyController.createSpecialty);
-
+router.post('/', 
+    // checkAuth(Role.ADMIN, Role.SUPER_ADMIN), 
+    multerUpload.single("file"), 
+    validateRequest(SpecialtyValidation.createSpecialtyZodSchema),
+    SpecialtyController.createSpecialty);
 router.get('/', SpecialtyController.getAllSpecialties);
 router.delete('/:id', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), SpecialtyController.deleteSpecialty);
 
