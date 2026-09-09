bf01fecec457a558c3c2fb8058d13f5c01dc8747
tabassum009 <tabassum.islam009@gmail.com>
2026-09-09 16:17:40 +0200

    Searching, filtering doctor services, testing apis done

--- src/app/module/doctor/doctor.controller.ts
+++ src/app/module/doctor/doctor.controller.ts
@@ -15,7 +15,8 @@ const getAllDoctors = catchAsync(
             httpStatusCode: status.OK,
             success: true,
             message: "Doctors fetched successfully",
-            data: result,
+            data: result.data,
+            meta: result.meta
         })
     }
 )
